32a2967777a81382a940ed53a65dbea6f3c600e3
Ricardo Vazquez Mendoza <r_vazquezmendoza@u.pacific.edu>
2023-11-09 00:50:08 -0800

    Started Map Design

--- Assets/Scripts/ShootingController.cs
+++ Assets/Scripts/ShootingController.cs
@@ -6,38 +6,15 @@ public class ShootingController : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform bulletSpawnPoint;
-    [SerializeField] private float bulletSpeed = 10f;
-    [SerializeField] private float shootingCooldown = 0.5f;
+    [SerializeField] private float attackRange = 5f; // Set the attack range in the Inspector
+    [SerializeField] private float shootingCooldown = 0.5f; // Time delay between shots
 
     private float lastShotTime;
 
-    // Track the currently equipped weapon
-    //private BaseWeapon currentWeapon; // You should reference the actual weapon script here
-
-    void Start()
-    {
-        // Initialize the current weapon (e.g., set it to the melee weapon initially)
-        // Replace 'MeleeWeapon' with the actual script/class for your melee weapon
-        //currentWeapon = GetComponent<MeleeWeapon>();
-    }
-
     void Update()
     {
-        HandleShootingInput();
+        RotateTowardsMouse();
 
-        // Handle weapon zooming
-        if (Input.GetButtonDown("Fire2"))
-        {
-            ZoomIn();
-        }
-        else if (Input.GetButtonUp("Fire2"))
-        {
-            ZoomOut();
-        }
-    }
-
-    void HandleShootingInput()
-    {
         if (Input.GetButtonDown("Fire1") && Time.time - lastShotTime >= shootingCooldown)
         {
             lastShotTime = Time.time;
@@ -45,22 +22,23 @@ public class ShootingController : MonoBehaviour
         }
     }
 
+    void RotateTowardsMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
     void Shoot()
     {
-        // if (currentWeapon != null)
-        // {
-        //     // Shoot using the current weapon
-        //     currentWeapon.Shoot();
-        // }
+        SpawnBullet();
     }
 
-    void ZoomIn()
+    void SpawnBullet()
     {
-        // Implement zooming logic here
-    }
-
-    void ZoomOut()
-    {
-        // Implement zooming out logic here
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
+        bulletController.InitializeBullet(bulletSpawnPoint.position, bulletSpawnPoint.rotation);
     }
 }
